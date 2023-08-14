1735183a1c3dcc8dbbfff352e87af68e971179a4
Antonio <antonioareas@me.com>
2023-08-14 12:49:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input("Pick a fruit")
 if add_my_fruit:
     my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + add_my_fruit + "')")
+    my_data_rows = my_cur.fetchall()
